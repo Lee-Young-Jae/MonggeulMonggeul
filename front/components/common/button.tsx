897df7bf24ca8e03cebcff9362fa010264f0187b
Lee-Young-Jae <yjlee1937@naver.com>
2023-06-23 21:55:20 +0900

    feat: 버튼 공용 컴포넌트 구현

--- front/components/common/button.tsx
+++ front/components/common/button.tsx
@@ -1,9 +1,59 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import styled, { css } from "styled-components";
+
+interface ButtonStyleProps {
   children: React.ReactNode;
+  isOutlined?: boolean;
+  size?: "s" | "m" | "l";
 }
 
-const Button = ({ ...props }: ButtonProps) => {
-  return <button {...props}></button>;
+const ButtonStyle = styled.button<ButtonStyleProps>`
+  width: 100%;
+  border: none;
+  border-radius: 15px;
+  padding: 0.5rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  color: white;
+
+  background-color: #f8c6d2;
+  font-family: inherit;
+  font-size: 1rem;
+
+  ${(props) =>
+    props.isOutlined &&
+    css`
+      background-color: white;
+      border: 1px solid #f8c6d2;
+      color: rgb(151, 151, 151);
+    `}
+
+  ${(props) =>
+    props.size === "s" &&
+    css`
+      width: 30%;
+    `}
+  ${(props) =>
+    props.size === "m" &&
+    css`
+      width: 50%;
+    `}  
+  ${(props) =>
+    props.size === "l" &&
+    css`
+      width: 100%;
+    `}
+`;
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  isOutlined?: boolean;
+  size?: "s" | "m" | "l";
+}
+
+const Button = ({ isOutlined, size, ...props }: ButtonProps) => {
+  return (
+    <ButtonStyle {...props} isOutlined={isOutlined} size={size}></ButtonStyle>
+  );
 };
 
 export default Button;

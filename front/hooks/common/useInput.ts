c5d2cac20160c5ea3c5a14ad3e426ecdebab28a6
Lee-Young-Jae <yjlee1937@naver.com>
2023-06-24 21:29:07 +0900

    feat: Input Hook 생성

---
+++ front/hooks/common/useInput.ts
@@ -0,0 +1,21 @@
+import { ChangeEvent, useState, ChangeEventHandler } from "react";
+
+type Elements = HTMLInputElement | HTMLSelectElement;
+
+function useInput(
+  initialValue = ""
+): [typeof value, ChangeEventHandler<Elements>, () => void] {
+  const [value, setValue] = useState(initialValue);
+
+  const handleValue = (event: ChangeEvent<Elements>) => {
+    setValue(event.target.value);
+  };
+
+  const resetValue = () => {
+    setValue("");
+  };
+
+  return [value, handleValue, resetValue];
+}
+
+export default useInput;

551c98c2880c7eb5f6652ae1e210c6e9a7e3c7dd
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-03 23:19:17 +0900

    feat: Recoil과 React-Query Provider 추가

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -2,11 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AppLayout from "../components/layout/AppLayout";
 import "./font.css";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }

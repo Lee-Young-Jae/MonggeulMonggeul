174c2bc1c863b6229032ded8532cac48cddc3baf
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-04 23:18:41 +0900

    feat: 그룹 생성 api 구현

---
+++ back/routes/group.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const router = express.Router();
+const { Group, User } = require("../models");
+const { isLoggedIn } = require("./middlewares");
+
+// 유저의 그룹 목록 send GET http://localhost:3010/group
+router.get("/", isLoggedIn, (req, res) => {
+  req.user.getGroups().then((groups) => {
+    res.status(200).json(groups);
+  });
+});
+
+// 그룹 생성 POST http://localhost:3010/group/create
+router.post("/create", isLoggedIn, async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name || name === "") {
+      return res.status(409).json({ message: "유효한 이름을 입력해주세요." });
+    }
+
+    const generateCode = () => {
+      return (
+        Math.random().toString(36).substr(2, 16) +
+        Math.random().toString(36).substr(2, 16)
+      ).toUpperCase();
+    };
+
+    let code = generateCode();
+    let existGroupCode = await Group.findOne({
+      where: { code: code },
+    });
+
+    while (existGroupCode) {
+      code = generateCode();
+      existGroupCode = Group.findOne({
+        where: { code: code },
+      })
+        ? true
+        : false;
+      if (!existGroupCode) {
+        break;
+      }
+    }
+
+    Group.create({
+      name: name,
+      code: code,
+    }).then((group) => {
+      req.user.addGroup(group);
+      res.status(201).json(group);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 그룹 가입 POST http://localhost:3010/group/join
+router.post("/join", isLoggedIn, async (req, res) => {
+  try {
+    const { code } = req.body;
+    if (!code || code === "") {
+      res.status(404).send({ message: "그룹 코드를 입력해주세요." });
+    }
+
+    const existGroup = await Group.findOne({
+      where: { code: code },
+    });
+    const existJoinedGroup = await req.user.getGroups({
+      where: { id: existGroup.id },
+    });
+
+    if (existGroup && existJoinedGroup.length === 0) {
+      req.user.addGroup(existGroup);
+      res.status(201).json(existGroup);
+    } else {
+      res
+        .status(404)
+        .send({ message: "이미 가입된 그룹이거나 그룹을 찾을 수가 없어요." });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+module.exports = router;

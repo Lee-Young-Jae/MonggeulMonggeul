78a367ccab37035f807b65b84f8853d09184fec0
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-05 21:03:50 +0900

    feat: Error 처리 컴포넌트 추가

---
+++ front/components/common/errorFallback.tsx
@@ -0,0 +1,72 @@
+import { ErrorResponse } from "@/types/axios";
+import { AxiosError } from "axios";
+import React from "react";
+import Modal from "./modal";
+import { useLottie } from "lottie-react";
+import notFoundLottie from "@/assets/not-found-animation.json";
+import styled from "styled-components";
+import Button from "@/components/common/button";
+import { useRouter } from "next/router";
+
+const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+`;
+
+const NavigationButton = styled(Button)`
+  background-color: #ffba00;
+`;
+
+interface ErrorFallbackProps {
+  error: AxiosError<ErrorResponse>;
+  resetErrorBoundary: (...args: Array<unknown>) => void;
+}
+
+const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
+  const { message } = error.response?.data || { message: "알 수 없는 오류" };
+  const { status } = error.response || { status: 500 };
+
+  const notFoundAnimation = useLottie({
+    animationData: notFoundLottie,
+    loop: true,
+    autoplay: true,
+    width: "20rem",
+  });
+  const router = useRouter();
+  const isNotAuthorized = status === 401 || status === 403;
+
+  const onClickHandler = () => {
+    if (isNotAuthorized) {
+      router.push("/");
+      resetErrorBoundary();
+    } else {
+      resetErrorBoundary();
+    }
+  };
+
+  return (
+    <NotFoundWrapper>
+      {notFoundAnimation.View}
+      찾을 수 없는 페이지네요... 요청하신 페이지가 사라졌거나, 잘못된 경로로
+      들어오신것 같아요
+      <NavigationButton
+        size="m"
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        메인 페이지로 갈게요
+      </NavigationButton>
+      <Modal visible={true} onClose={onClickHandler}>
+        <p>{message}</p>
+        <Button onClick={onClickHandler}>돌아가기</Button>
+      </Modal>
+    </NotFoundWrapper>
+  );
+};
+
+export default ErrorFallback;

ca92471434c1a499d6a13176e14bd6c689e49da3
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-06 14:46:46 +0900

    feat: getGroup에 에러바운더리 설정 추가

--- front/hooks/queries/group/useGet.ts
+++ front/hooks/queries/group/useGet.ts
@@ -1,5 +1,6 @@
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import { fetchGetUserGroups } from "@/apis/user.api";
+import { getGroup } from "@/apis/group.api";
 import * as GroupType from "@/types/group";
 import { ErrorResponse } from "@/types/axios";
 
@@ -9,8 +10,25 @@ const useGetUserGroups = (
   return useQuery<GroupType.Group[], ErrorResponse>(
     ["UserGroups"],
     fetchGetUserGroups,
-    queryOptions
+    {
+      useErrorBoundary: true,
+      ...queryOptions,
+    }
   );
 };
 
-export { useGetUserGroups };
+const useGetGroup = (
+  groupCode: string,
+  queryOptions?: UseQueryOptions<GroupType.Group, ErrorResponse>
+) => {
+  return useQuery<GroupType.Group, ErrorResponse>(
+    ["Group", groupCode],
+    () => getGroup(groupCode),
+    {
+      useErrorBoundary: true,
+      ...queryOptions,
+    }
+  );
+};
+
+export { useGetUserGroups, useGetGroup };

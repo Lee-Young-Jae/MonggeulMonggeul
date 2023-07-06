bd05c60bc17a8343501340f75b5830cb4c78b193
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-06 14:46:13 +0900

    feat: 그룹 가입 비동기 함수 작성

---
+++ front/apis/group.api.ts
@@ -0,0 +1,25 @@
+import axiosInstance from "./config/axiosInstance";
+import * as GroupType from "@/types/group";
+
+export const createGroup = async (
+  groupName: string
+): Promise<GroupType.Group> => {
+  const response = await axiosInstance.post(`/group/create`, {
+    name: groupName,
+  });
+  return response.data;
+};
+
+export const joinGroup = async (
+  groupCode: string
+): Promise<GroupType.Group> => {
+  const response = await axiosInstance.post(`/group/join`, {
+    code: groupCode,
+  });
+  return response.data;
+};
+
+export const getGroup = async (groupCode: string): Promise<GroupType.Group> => {
+  const response = await axiosInstance.get(`/group/${groupCode}`);
+  return response.data;
+};

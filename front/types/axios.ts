84bdae543b9f0e81c721c3eef7f12db5c3773b2e
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-07 13:42:31 +0900

    feat: Error 타입 추가

--- front/types/axios.ts
+++ front/types/axios.ts
@@ -1,3 +1,10 @@
+import { UseMutationOptions } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
-export type ErrorResponse = AxiosError<{ message: string }> | Error;
+export type ErrorResponse = AxiosError<{ message: string }, any> | Error;
+
+export type UseCustomMutationOptions<TData = unknown> = UseMutationOptions<
+  TData,
+  ErrorResponse,
+  unknown
+>;

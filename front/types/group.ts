bb58ec4eca010a3f8983a47825f7dfdcaea56486
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-10 16:59:52 +0900

    feat: group 타입에 Users 추가

--- front/types/group.ts
+++ front/types/group.ts
@@ -1,8 +1,11 @@
+import { User } from "./user";
+
 type Group = {
   id: number;
   name: string;
   code: string;
   createdAt: string;
+  Users: User[];
 };
 
 type createGroupRequest = Pick<Group, "name" | "code">;

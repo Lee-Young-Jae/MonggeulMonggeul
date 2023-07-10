2025dff81b8fccef9a7bc189f4c2a38a674c27a0
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-10 19:22:21 +0900

    feat: react-query 요청 실패 시 1번만 재시도 하도록 기본 옵션 수정

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -6,7 +6,14 @@ import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // 요청 실패 시 1번 재시도
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

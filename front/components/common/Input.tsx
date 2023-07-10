bdf44d3bb6a09c8b8dccfe195f3d39d0f62c5d4f
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-10 16:58:55 +0900

    feat: Input 공통 컴포넌트 추가

---
+++ front/components/common/Input.tsx
@@ -0,0 +1,44 @@
+import styled, { css } from "styled-components";
+
+interface InputStyleProps {
+  outlined?: "true" | "false";
+}
+
+const InputStyle = styled.input<InputStyleProps>`
+  border: none;
+  outline: none;
+  font-size: 1rem;
+  text-align: center;
+  margin-top: 1rem;
+  border-radius: 14px;
+  padding: 0.5rem;
+  border: 1px solid #f8c6d2;
+  font-family: inherit;
+
+  ${(props) =>
+    props.outlined === "true" &&
+    css`
+      border: 1px solid #f4356c;
+    `}
+
+  ${(props) =>
+    props.outlined === "false" &&
+    css`
+      border: 1px solid #f8c6d2;
+    `}
+`;
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  outlined?: boolean;
+}
+
+const Input = ({ outlined, ...props }: InputProps) => {
+  return (
+    <InputStyle
+      outlined={outlined === true ? "true" : "false"}
+      {...props}
+    ></InputStyle>
+  );
+};
+
+export default Input;

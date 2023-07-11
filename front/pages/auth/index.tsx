72f738c36c107b806a251755da24f813c8ff870d
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-11 18:02:26 +0900

    feat: 로그인 여부에 따른 분기 처리

---
+++ front/pages/auth/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useAuth } from "@/hooks/common/useAuth";
+import Loading from "@/components/common/loading";
+import { useGetUserGroups } from "@/hooks/queries/group/useGet";
+
+const AuthPage = () => {
+  const router = useRouter();
+  const { isLogin, getLoginInfo } = useAuth();
+  const { isLoading: isLoginLoading } = getLoginInfo;
+  const { data: groups } = useGetUserGroups();
+
+  useEffect(() => {
+    const userGroups = groups ?? [];
+
+    if (!isLogin && !isLoginLoading) {
+      router.push("/");
+    } else if (isLogin && userGroups?.length > 0) {
+      router.push(`/groups/${userGroups[0].code}`, undefined, {
+        shallow: true,
+      });
+    } else if (isLogin && userGroups.length === 0) {
+      router.push("/groups", undefined, { shallow: true });
+    } else {
+      console.log("로딩중...");
+    }
+  }, [isLogin, router, isLoginLoading, groups]);
+
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
+};
+
+export default AuthPage;

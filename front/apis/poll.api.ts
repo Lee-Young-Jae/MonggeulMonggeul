25101685f499236fd358a74591feb10dc97aaac2
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-14 17:44:39 +0900

    feat: 투표 삭제 api 구현

---
+++ front/apis/poll.api.ts
@@ -0,0 +1,24 @@
+import axiosInstance from "./config/axiosInstance";
+import { Poll, createPollRequest, createPollResponse } from "@/types/poll";
+
+export const getPolls = async (groupCode: string): Promise<Poll[]> => {
+  const response = await axiosInstance.get(`/poll?groupcode=${groupCode}`);
+  return response.data;
+};
+
+export const createPoll = async (
+  createPollRequest: createPollRequest
+): Promise<createPollResponse> => {
+  const response = await axiosInstance.post(`/poll/create`, {
+    ...createPollRequest,
+  });
+  return response.data;
+};
+
+// FIX 반환 타입 수정 필요
+export const deletePoll = async (pollId: number): Promise<string> => {
+  const response = await axiosInstance.delete(`/poll/delete`, {
+    data: { pollId },
+  });
+  return response.data;
+};

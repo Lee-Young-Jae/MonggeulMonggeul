a74f42e6a067fd2bc896e52da8f9a1ffbe1a5853
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-18 23:31:22 +0900

    refact: 기능에 어울리는 이름으로 파일명 변경

---
+++ front/pages/groups/[groupcode]/poll/components/pollSelectList.tsx
@@ -0,0 +1,34 @@
+import Radio from "@/components/common/radio";
+import RadioGroup from "@/components/common/radio/radioGroup";
+import { subject } from "@/types/poll";
+import React from "react";
+import styled from "styled-components";
+
+const RadioStyle = styled(Radio)`
+  margin-right: 3rem;
+`;
+
+interface PollSubjectProps {
+  PollSubjects: subject[];
+}
+
+const PollSelectList = ({ PollSubjects }: PollSubjectProps) => {
+  return (
+    <RadioGroup label="어디에 투표할까요?">
+      {PollSubjects.map((subject, index) => {
+        return (
+          <RadioStyle
+            key={subject.id}
+            name={subject.title}
+            value={subject.title}
+            onChange={() => {}}
+          >
+            {subject.title}
+          </RadioStyle>
+        );
+      })}
+    </RadioGroup>
+  );
+};
+
+export default PollSelectList;

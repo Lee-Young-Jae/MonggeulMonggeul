b0a60db6a5cb3c5ae05df161bf728cc91fba4047
Lee-Young-Jae <yjlee1937@naver.com>
2023-07-25 23:48:47 +0900

    feat: 체크박스 컴포넌트 작성

--- front/components/common/checkList/index.tsx
+++ front/components/common/checkList/index.tsx
@@ -1,31 +1,76 @@
 import React from "react";
+import styled from "styled-components";
 
-interface CheckListProps {
-  checkList: string[];
-  setCheckList: React.Dispatch<React.SetStateAction<string[]>>;
+const CheckBoxStyle = styled.label`
+  cursor: pointer;
+  -webkit-touch-callout: none;
+  user-select: none;
+
+  input[type="checkbox"] {
+    display: none;
+  }
+
+  input[type="checkbox"] + span {
+    display: inline-block;
+    display: flex;
+    align-items: center;
+    position: relative;
+  }
+
+  // 체크하기 전
+  input[type="checkbox"] + span::before {
+    content: "";
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border: 1px solid #ccc;
+    color: white;
+    border-radius: 0.2rem;
+    transition: 0.1s all ease-in-out;
+  }
+
+  // 체크한 후
+  input[type="checkbox"]:checked + span::before {
+    content: "✔";
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 0.2rem;
+    background: #f4356c;
+    color: white;
+    text-align: center;
+    line-height: 1rem;
+    transition: 0.1s all ease-in-out;
+  }
+`;
+
+interface CheckBoxProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  checked?: boolean;
+  handler?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
 
-const CheckList = ({ checkList, setCheckList }: CheckListProps) => {
+const CheckList = ({
+  children,
+  disabled,
+  checked,
+  handler,
+  value,
+}: CheckBoxProps) => {
   return (
-    <div>
-      {checkList.map((check, index) => {
-        return (
-          <div key={index}>
-            <input
-              type="checkbox"
-              checked={check === "true"}
-              value={check}
-              onChange={(e) => {
-                const newCheckList = [...checkList];
-                newCheckList[index] = e.target.checked.toString();
-                setCheckList(newCheckList);
-              }}
-            />
-            <span>{check}</span>
-          </div>
-        );
-      })}
-    </div>
+    <CheckBoxStyle>
+      <input
+        type="checkbox"
+        disabled={disabled}
+        checked={checked}
+        onChange={handler}
+        value={value}
+      />
+      <span>{children}</span>
+    </CheckBoxStyle>
   );
 };
 

ab8777c9de04c1b7de60d0638184c7f65c18f339
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-04 22:51:24 +0900

    feat: 유저의 모임 목록 가져오기 라우터에서 User 모델도 가져오는 기능 추가

--- back/routes/group.js
+++ back/routes/group.js
@@ -3,18 +3,28 @@ const router = express.Router();
 const { Group, User } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 
-// 유저의 그룹 목록 send GET http://localhost:3010/group
+// 유저의 모임 목록 send GET http://localhost:3010/group
 router.get("/", isLoggedIn, (req, res) => {
-  req.user.getGroups().then((groups) => {
-    res.status(200).json(groups);
-  });
+  req.user
+    .getGroups({
+      attributes: ["id", "name", "code"],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "name", "email", "profileImage"],
+        },
+      ],
+    })
+    .then((groups) => {
+      res.status(200).json(groups);
+    });
 });
 
-// 그룹 정보 GET http://localhost:3010/group/:id
+// 모임 정보 GET http://localhost:3010/group/:id
 router.get("/:code", isLoggedIn, async (req, res) => {
   try {
     if (!req.params.code || req.params.code === "") {
-      return res.status(409).json({ message: "유효한 그룹을 선택해주세요." });
+      return res.status(409).json({ message: "유효한 모임을 선택해주세요." });
     }
 
     const group = await Group.findOne({
@@ -27,7 +37,7 @@ router.get("/:code", isLoggedIn, async (req, res) => {
       ],
     });
     if (!group) {
-      return res.status(404).json({ message: "그룹을 찾을 수 없습니다." });
+      return res.status(404).json({ message: "모임을 찾을 수 없습니다." });
     }
     res.status(200).json(group);
   } catch (error) {
@@ -35,7 +45,7 @@ router.get("/:code", isLoggedIn, async (req, res) => {
   }
 });
 
-// 그룹 생성 POST http://localhost:3010/group/create
+// 모임 생성 POST http://localhost:3010/group/create
 router.post("/create", isLoggedIn, async (req, res) => {
   try {
     const { name } = req.body;
@@ -79,12 +89,12 @@ router.post("/create", isLoggedIn, async (req, res) => {
   }
 });
 
-// 그룹 가입 POST http://localhost:3010/group/join
+// 모임 가입 POST http://localhost:3010/group/join
 router.post("/join", isLoggedIn, async (req, res) => {
   try {
     const { code } = req.body;
     if (!code || code === "") {
-      res.status(404).send({ message: "그룹 코드를 입력해주세요." });
+      res.status(404).send({ message: "모임 코드를 입력해주세요." });
     }
 
     const existGroup = await Group.findOne({
@@ -105,37 +115,37 @@ router.post("/join", isLoggedIn, async (req, res) => {
     } else {
       res
         .status(404)
-        .send({ message: "이미 가입된 그룹이거나 그룹을 찾을 수가 없어요." });
+        .send({ message: "이미 가입된 모임이거나 모임을 찾을 수가 없어요." });
     }
   } catch (err) {
     console.error(err);
   }
 });
 
-//그룹 탈퇴 POST http://localhost:3010/group/leave
+//모임 탈퇴 POST http://localhost:3010/group/leave
 router.post("/leave", isLoggedIn, async (req, res) => {
   try {
     const { code } = req.body;
     if (!code || code === "") {
-      res.status(404).send({ message: "유효한 그룹이 아닙니다." });
+      res.status(404).send({ message: "유효한 모임이 아닙니다." });
     }
-    // 그룹 찾기
+    // 모임 찾기
     const existGroup = await Group.findOne({
       where: { code: code },
     });
 
     if (!existGroup) {
-      res.status(404).send({ message: "유효한 그룹이 아닙니다." });
+      res.status(404).send({ message: "유효한 모임이 아닙니다." });
     }
 
-    // 유저가 가입된 그룹인지 확인
+    // 유저가 가입된 모임인지 확인
     const existJoinedGroup = await req.user.getGroups({
       where: { id: existGroup.id },
     });
 
     if (existGroup && existJoinedGroup.length !== 0) {
       req.user.removeGroup(existGroup);
-      // 그룹에 유저가 없으면 그룹 삭제
+      // 모임에 유저가 없으면 모임 삭제
       const existUser = await existGroup.getUsers();
       if (existUser.length === 1) {
         await Group.destroy({
@@ -146,10 +156,10 @@ router.post("/leave", isLoggedIn, async (req, res) => {
     }
 
     if (existGroup && existJoinedGroup.length === 0) {
-      res.status(404).send({ message: "가입된 그룹이 아닙니다." });
+      res.status(404).send({ message: "가입된 모임이 아닙니다." });
     }
 
-    res.status(404).send({ message: "유효한 그룹이 아닙니다." });
+    res.status(404).send({ message: "유효한 모임이 아닙니다." });
   } catch (err) {
     console.error(err);
   }

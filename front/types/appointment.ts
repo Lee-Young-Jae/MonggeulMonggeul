a0b87cd5326adcc46dce18c834169f9e6491ea1b
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-08 23:28:20 +0900

    feat: 약속잡기 타입 추가

---
+++ front/types/appointment.ts
@@ -0,0 +1,32 @@
+import { User } from "./user";
+
+type Appointment = {
+  id: number;
+  title: string;
+  sub_title: string;
+  start_date: Date;
+  end_date: Date;
+  start_time: string;
+  end_time: string;
+  code: string;
+  status: string;
+  deadline: Date;
+  duration_minutes: number;
+  created_at: Date;
+  group_code: string;
+  host_id: number;
+};
+
+type createAppointmentRequest = Pick<
+  Appointment,
+  | "title"
+  | "sub_title"
+  | "start_date"
+  | "end_date"
+  | "start_time"
+  | "end_time"
+  | "deadline"
+  | "duration_minutes"
+  | "group_code"
+>;
+export type { Appointment, createAppointmentRequest };

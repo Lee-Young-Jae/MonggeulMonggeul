2b7d25b364ea1ead1458480faac67b0b91eda9f8
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-12 19:22:40 +0900

    feat: 약속잡기 생성 api 작성

--- front/apis/appointment.api.ts
+++ front/apis/appointment.api.ts
@@ -1,11 +1,21 @@
 import axiosInstance from "./config/axiosInstance";
-import { Appointment } from "@/types/appointment";
+import { Appointment, createAppointmentRequest } from "@/types/appointment";
 
 export const getAppointments = async (
   groupCode: string
 ): Promise<Appointment[]> => {
   const response = await axiosInstance.get(
     `/appointment?groupcode=${groupCode}`
+  );
+  return response.data;
+};
+
+export const createAppointment = async (
+  createAppointmentRequest: createAppointmentRequest
+): Promise<Appointment> => {
+  const response = await axiosInstance.post(
+    "/appointment",
+    createAppointmentRequest
   );
   return response.data;
 };

749f4c391c22fc922b8a0cc1467c653e24e04370
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-12 19:23:30 +0900

    fix: 시작&끝 시간을 시작&끝 날짜와 시간으로 분리

--- back/models/appointment.js
+++ back/models/appointment.js
@@ -10,14 +10,24 @@ class Appointment extends Model {
           type: DataTypes.STRING(200),
           allowNull: false,
         },
-        // 가능 시간 start 0000 ~ 2359
-        start_date_time: {
+        // 가능 날짜 start
+        start_date: {
           type: DataTypes.DATE,
           allowNull: false,
         },
-        // 가능 시간 end 0000 ~ 2359
-        end_date_time: {
+        // 가능 날짜 end
+        end_date: {
           type: DataTypes.DATE,
+          allowNull: false,
+        },
+        // 가능 시간 start 00:00 ~ 23:59
+        start_time: {
+          type: DataTypes.TIME,
+          allowNull: false,
+        },
+        // 가능 시간 end 00:00 ~ 23:59
+        end_time: {
+          type: DataTypes.TIME,
           allowNull: false,
         },
         // appointment 고유 코드
@@ -26,7 +36,7 @@ class Appointment extends Model {
           allowNull: false,
           unique: true,
         },
-        // 진행상태
+        // 진행상태  "진행중", "마감", "취소"
         status: {
           type: DataTypes.STRING(10),
           allowNull: false,

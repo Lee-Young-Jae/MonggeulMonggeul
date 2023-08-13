0c5729a72e48c5c3511f9abe892fe7607a410740
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-13 19:17:01 +0900

    feat: 약속 목록 컴포넌트 작성

---
+++ front/pages/groups/[groupcode]/appointment/components/appointmentList.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import AppointmentItem from "./appointmentItem";
+import { Appointment } from "@/types/appointment";
+
+const StyledAppointmentList = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+interface IProps {
+  appointments: Appointment[];
+}
+
+const AppointmentList = ({ appointments }: IProps) => {
+  return (
+    <StyledAppointmentList>
+      {appointments.map((appointment) => (
+        <AppointmentItem
+          key={appointment.id}
+          appointment={appointment}
+        ></AppointmentItem>
+      ))}
+    </StyledAppointmentList>
+  );
+};
+
+export default AppointmentList;

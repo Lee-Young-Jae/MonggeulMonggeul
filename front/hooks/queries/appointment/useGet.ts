bcd15b015c819c5d93f920dd84b2d6b8327101c5
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-13 19:15:36 +0900

    refact: 훅의 인자로 options를 받아 확장성을 높인다.

--- front/hooks/queries/appointment/useGet.ts
+++ front/hooks/queries/appointment/useGet.ts
@@ -1,15 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
+import { UseQueryOptions, useQuery } from "@tanstack/react-query";
 import {} from "@/apis/user.api";
 import { getAppointments } from "@/apis/appointment.api";
 import * as AppointmentType from "@/types/appointment";
+import { ErrorResponse } from "@/types/axios";
 
-const useGetAppointments = (groupCode: string) => {
-  return useQuery<AppointmentType.Appointment[]>(
+const useGetAppointments = (
+  groupCode: string,
+  queryOptions?: UseQueryOptions<AppointmentType.Appointment[], ErrorResponse>
+) => {
+  return useQuery<AppointmentType.Appointment[], ErrorResponse>(
     ["Appointments", groupCode],
     () => getAppointments(groupCode),
     {
       useErrorBoundary: true,
       staleTime: 30000,
+      ...queryOptions,
     }
   );
 };

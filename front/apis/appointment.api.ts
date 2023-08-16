fb34c064d4a86e9c7f5d48c57df6ea7d259c34f6
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-16 20:21:51 +0900

    feat: 약속 상태 업데이트 api 추가

--- front/apis/appointment.api.ts
+++ front/apis/appointment.api.ts
@@ -3,6 +3,7 @@ import {
   Appointment,
   createAppointmentRequest,
   deleteAppointmentResponse,
+  updateAppointmentStatusRequest,
 } from "@/types/appointment";
 
 export const getAppointments = async (
@@ -11,6 +12,13 @@ export const getAppointments = async (
   const response = await axiosInstance.get(
     `/appointment?groupcode=${groupCode}`
   );
+  return response.data;
+};
+
+export const getAppointment = async (
+  code: Appointment["code"]
+): Promise<Appointment> => {
+  const response = await axiosInstance.get(`/appointment/${code}`);
   return response.data;
 };
 
@@ -30,5 +38,16 @@ export const deleteAppointment = async (
   const response = await axiosInstance.delete(`/appointment/`, {
     data: { code },
   });
+  return response.data;
+};
+
+export const updateAppointmentStatus = async (
+  updateAppointmentStatusRequest: updateAppointmentStatusRequest
+): Promise<Appointment> => {
+  console.log(updateAppointmentStatusRequest);
+  const response = await axiosInstance.put(
+    `/appointment/${updateAppointmentStatusRequest.code}`,
+    { status: updateAppointmentStatusRequest.status }
+  );
   return response.data;
 };

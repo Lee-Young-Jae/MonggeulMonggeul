e73f07e718eabe95a3f5ba1379eaada1448c8640
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-19 16:23:48 +0900

    feat: 약속시간에 투표 Api 작성

--- back/routes/appointment.js
+++ back/routes/appointment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Group, User, Appointment } = require("../models");
+const { Group, User, Appointment, AppointmentTimeVote } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 const { generateRandomCode } = require("../utils/common");
 
@@ -201,6 +201,117 @@ router.put("/:code", isLoggedIn, async (req, res) => {
       return res.status(404).json({ message: "약속을 찾을 수 없습니다." });
     }
     res.status(200).json({ message: "약속 상태가 변경되었습니다." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "서버에서 에러가 발생했어요 잠시 후 다시 시도해 주세요.",
+    });
+  }
+});
+
+// 유저가 약속시간에 투표 POST http://localhost:3010/appointment/:code/vote
+router.post("/vote", isLoggedIn, async (req, res) => {
+  try {
+    const { code, pickTimes } = req.body;
+
+    const existAppointment = await Appointment.findOne({
+      where: { code },
+    });
+
+    if (!existAppointment) {
+      return res.status(404).json({ message: "약속을 찾을 수 없습니다." });
+    }
+
+    const times = [];
+    for (let date in pickTimes) {
+      const value = pickTimes[date];
+      if (value) {
+        value.forEach((time) => {
+          times.push([date, time]);
+        });
+      }
+    }
+
+    const createdAppointmentTimeVote = await AppointmentTimeVote.bulkCreate(
+      times.map((time) => {
+        return {
+          selectedDate: new Date(time[0] + " " + time[1]),
+          appointmentCode: code,
+          userId: req.user.id,
+        };
+      })
+    );
+
+    if (!createdAppointmentTimeVote) {
+      return res.status(500).json({
+        message: "서버에서 에러가 발생했어요 잠시 후 다시 시도해 주세요.",
+      });
+    }
+
+    res.status(201).json(createdAppointmentTimeVote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "서버에서 에러가 발생했어요 잠시 후 다시 시도해 주세요.",
+    });
+  }
+});
+
+// 가장 많이 겹치는 시간대 조회 GET http://localhost:3010/appointment/:code/vote/rank
+router.get("/:code/vote/rank", isLoggedIn, async (req, res) => {
+  try {
+    const { code } = req.params;
+
+    const existAppointment = await Appointment.findOne({
+      where: { code },
+    });
+
+    if (!existAppointment) {
+      return res.status(404).json({ message: "약속을 찾을 수 없습니다." });
+    }
+
+    // 모임의 멤버인지 확인
+    const existGroup = await Group.findOne({
+      where: { code: existAppointment.groupCode },
+    });
+    const isJoinedUser = await existGroup.hasUser(req.user.id);
+
+    if (!isJoinedUser) {
+      return res.status(403).json({ message: "권한이 없습니다." });
+    }
+
+    const appointmentTimeVotes = await AppointmentTimeVote.findAll({
+      where: { appointmentCode: code },
+    });
+
+    if (!appointmentTimeVotes) {
+      return res.status(200).json([]);
+    }
+
+    const timeVotes = {};
+    appointmentTimeVotes.forEach((timeVote) => {
+      const date = timeVote.dataValues.selectedDate;
+      const dateStr = date.toISOString().split("T")[0];
+      const timeStr = date.toISOString().split("T")[1].split(".")[0];
+      if (timeVotes[dateStr]) {
+        timeVotes[dateStr].push(timeStr);
+      } else {
+        timeVotes[dateStr] = [timeStr];
+      }
+    });
+
+    const timeVoteRank = [];
+    for (let date in timeVotes) {
+      const value = timeVotes[date];
+      const count = value.length;
+      timeVoteRank.push({ date, count, times: value });
+    }
+
+    timeVoteRank.sort((a, b) => {
+      return b.count - a.count;
+    });
+
+    res.status(200).json(timeVoteRank);
   } catch (error) {
     console.error(error);
     res.status(500).json({

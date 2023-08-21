f598dea18ccb2ba99f1a408538e6309b12f2c4cd
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-21 18:13:04 +0900

    feat: 날짜와 분을 입력 받아 합친 날짜를 반환하는 공통함수 작성

--- front/utills/common.ts
+++ front/utills/common.ts
@@ -19,3 +19,38 @@ export const formatMinute = (minute: number) => {
   if (hour === 0) return `${min}분`;
   else return `${hour}시간 ${min}분`;
 };
+
+/**
+ *  시간의 합을 구하는 함수
+ * @param {string} time1 - 시간1
+ * @param {string} time2 - 시간2
+ * @returns {string} - 시간1 + 시간2
+ */
+export const addTime = (time1: string, time2: string) => {
+  const [hour1, minute1] = time1.split(":").map((time) => Number(time));
+  const [hour2, minute2] = time2.split(":").map((time) => Number(time));
+
+  const hour = hour1 + hour2;
+  const minute = minute1 + minute2;
+
+  return `${hour}:${minute}`;
+};
+
+/**
+ * 날짜와 분을 받아 합친 날짜를 반환하는 함수
+ * @param {Date} date - 날짜
+ * @param {number} minute - 분
+ * @returns {Date} - 날짜 + 분
+ * @example
+ * const date = new Date("2021-08-01T00:00:00");
+ * const minute = 30;
+ * const result = addMinute(date, minute);
+ * console.log(result); // 2021-08-01T00:30:00
+ * console.log(result instanceof Date); // true
+
+  */
+export const addMinute = (date: Date, minute: number): Date => {
+  const result = new Date(date);
+  result.setMinutes(result.getMinutes() + minute);
+  return result;
+};

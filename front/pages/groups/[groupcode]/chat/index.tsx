6ef0de288bcc8dc4b1b81f0aebfae6874d273350
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-25 17:18:04 +0900

    feat: 유저 퇴장 메세지가 채팅창에 출력되는 기능 추가

--- front/pages/groups/[groupcode]/chat/index.tsx
+++ front/pages/groups/[groupcode]/chat/index.tsx
@@ -11,11 +11,13 @@ import { socket } from "@/apis/config/socket.io";
 import { useGetMyProfile } from "@/hooks/queries/user/useGet";
 import { User } from "@/types/user";
 import Image from "next/image";
+import Loading from "@/components/common/loading";
 
 interface receivedMessageProps {
   message: string;
   time: string;
   user: User;
+  type: "chat" | "join" | "leave";
 }
 
 const ProfileImage = styled(Image)`
@@ -88,14 +90,29 @@ const ChatItemBody = styled.div`
   word-break: keep-all;
 `;
 
+const JoinMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  background-color: #f2f2f2;
+  border-radius: 5px;
+  color: #999999;
+  margin-top: 5px;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
 const Message = styled.div``;
 
 const Chat = () => {
   const router = useRouter();
   const { groupcode } = router.query;
-  const { data: userProfile } = useGetMyProfile({
-    staleTime: 1000 * 60 * 10,
-  });
+  const { data: userProfile, isSuccess: getUserProfileSuccess } =
+    useGetMyProfile({
+      staleTime: 1000 * 60 * 10,
+    });
 
   const [receivedMessages, setReceivedMessages] = useState<
     receivedMessageProps[]
@@ -105,15 +122,25 @@ const Chat = () => {
     {
       name: "chat",
       handler: (data: receivedMessageProps) => {
-        console.log(data);
         setReceivedMessages((prev) => [data, ...prev]);
       },
     },
 
     {
-      name: "connection",
-      handler: () => {
-        console.log("connected");
+      name: "joinedUser",
+      handler: (data: receivedMessageProps) => {
+        if (data) {
+          setReceivedMessages((prev) => [data, ...prev]);
+        }
+      },
+    },
+
+    {
+      name: "leavedUser",
+      handler: (data: receivedMessageProps) => {
+        if (data) {
+          setReceivedMessages((prev) => [data, ...prev]);
+        }
       },
     },
   ];
@@ -122,42 +149,76 @@ const Chat = () => {
 
   const sendMessage = (event: any) => {
     event.preventDefault();
-    emit("chat", { message, user: userProfile }, (error: any) => {
-      if (error) {
-        alert(error);
+    emit(
+      "chat",
+      { message, user: userProfile, roomId: groupcode },
+      (error: any) => {
+        if (error) {
+          alert(error);
+        }
       }
-    });
+    );
     resetMessage();
   };
 
   useEffect(() => {
     // groupcode로 socket 연결
+    if (!groupcode || !getUserProfileSuccess) return;
+
     socket.connect();
-  }, []);
+    socket.emit(
+      "join",
+      { roomId: groupcode, user: userProfile },
+      (error: any) => {
+        if (error) {
+          alert(error);
+        }
+      }
+    );
+
+    return () => {
+      socket.emit("leave", { roomId: groupcode, user: userProfile });
+      socket.disconnect();
+    };
+  }, [getUserProfileSuccess, groupcode]);
+
+  if (!getUserProfileSuccess) {
+    return <Loading />;
+  }
 
   return (
     <GroupPage>
       <PageContent>
         <ChatContainer>
-          {receivedMessages.map((message, index) => (
-            <ChatItem key={index}>
-              <ChatItemHeader>
-                <UserProfile>
-                  <ProfileImage
-                    width={35}
-                    height={35}
-                    src={message.user.profileImage}
-                    alt={message.user.profileImage}
-                  />
-                  <p>{message.user.name}</p>
-                </UserProfile>
-              </ChatItemHeader>
-              <ChatItemBody>
-                <Message>{message.message}</Message>
-                <TimeStamp>{message.time}</TimeStamp>
-              </ChatItemBody>
-            </ChatItem>
-          ))}
+          {receivedMessages.map((message, index) =>
+            message.type === "chat" ? (
+              <ChatItem key={index}>
+                <ChatItemHeader>
+                  <UserProfile>
+                    <ProfileImage
+                      width={35}
+                      height={35}
+                      src={message.user.profileImage}
+                      alt={message.user.profileImage}
+                    />
+                    <p>{message.user.name}</p>
+                  </UserProfile>
+                </ChatItemHeader>
+                <ChatItemBody>
+                  <Message>{message.message}</Message>
+                  <TimeStamp>{message.time}</TimeStamp>
+                </ChatItemBody>
+              </ChatItem>
+            ) : message.type === "join" ? (
+              <JoinMessage key={index}>
+                <p>{message?.user?.name}님이 입장하셨습니다.</p>
+              </JoinMessage>
+            ) : (
+              <JoinMessage key={index}>
+                <p>{message?.user?.name}님이 퇴장하셨습니다.</p>
+              </JoinMessage>
+            )
+          )}
         </ChatContainer>
 
         <form>

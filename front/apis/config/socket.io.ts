bce280245695f7a190995566bee6b98b27621360
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-25 17:15:16 +0900

    fix: autoConnect 옵션 활성화

--- front/apis/config/socket.io.ts
+++ front/apis/config/socket.io.ts
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 
 export const socket = io(process.env.NEXT_PUBLIC_API_URL as string, {
   transports: ["websocket"],
-  autoConnect: false,
+  autoConnect: true,
 });

7938c7d70a0871763279ace7b3ba1f02d9b0d5ce
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-25 17:14:51 +0900

    feat: 프로필 가져오기 api 작성

--- front/apis/user.api.ts
+++ front/apis/user.api.ts
@@ -1,7 +1,13 @@
 import axiosInstance from "./config/axiosInstance";
 import * as GroupType from "@/types/group";
+import * as UserType from "@/types/user";
 
 export const fetchGetUserGroups = async (): Promise<GroupType.Group[]> => {
   const response = await axiosInstance.get(`/group`);
+  return response.data;
+};
+
+export const getMyProfile = async (): Promise<UserType.User> => {
+  const response = await axiosInstance.get(`/user/profile`);
   return response.data;
 };

a44102cbb7c349cc23980d4715040f5fa2e06741
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-27 19:58:54 +0900

    refact: 훅 재사용을 위해 options를 외부에서 넘겨받도록 수정

--- front/hooks/common/useAuth.ts
+++ front/hooks/common/useAuth.ts
@@ -1,7 +1,12 @@
 import { useGetLoginInfo } from "@/hooks/queries/user/useGet";
+import { ErrorResponse } from "@/types/axios";
+import { UseQueryOptions } from "@tanstack/react-query";
+import * as UserType from "@/types/user";
 
-const useAuth = () => {
-  const getLoginInfo = useGetLoginInfo({ suspense: false });
+const useAuth = (
+  queryOptions?: UseQueryOptions<UserType.User, ErrorResponse>
+) => {
+  const getLoginInfo = useGetLoginInfo(queryOptions);
   const isLogin = getLoginInfo.isSuccess && getLoginInfo.data !== null;
 
   return { getLoginInfo, isLogin };

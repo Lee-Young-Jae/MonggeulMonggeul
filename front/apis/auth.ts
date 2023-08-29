f2b8a2340b3077e3c57153a6231324e0d4ed5306
Lee-Young-Jae <yjlee1937@naver.com>
2023-08-29 17:55:12 +0900

    feat: 로그인 요청 api 추가

--- front/apis/auth.ts
+++ front/apis/auth.ts
@@ -1,6 +1,11 @@
 import axiosInstance from "./config/axiosInstance";
 import * as UserType from "@/types/user";
 
+export const fetchLogin = async () => {
+  const response = await axiosInstance.get("/auth/kakao");
+  return response.data;
+};
+
 export const fetchGetLoginInfo = async (): Promise<UserType.User> => {
   const response = await axiosInstance.get("/auth");
 

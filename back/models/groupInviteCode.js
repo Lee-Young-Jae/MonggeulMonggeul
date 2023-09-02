93f0da1501fd1ab0aa9d23a580da01066105aca7
Lee-Young-Jae <yjlee1937@naver.com>
2023-09-02 23:44:51 +0900

    feat: 그룹 초대 코드 모델 생성

---
+++ back/models/groupInviteCode.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+class GroupInviteCode extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        code: {
+          type: DataTypes.STRING(25),
+          allowNull: false,
+          unique: true,
+        },
+
+        expiredAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+
+        status: {
+          type: DataTypes.STRING(10),
+          allowNull: false,
+          defaultValue: "valid",
+
+          validate: {
+            isIn: [["valid", "invalid"]],
+          },
+
+          comment: "valid: 유효한 초대 코드, invalid: 유효하지 않은 초대 코드",
+
+          get() {
+            return this.getDataValue("status");
+          },
+        },
+      },
+      {
+        modelName: "GroupInviteCode",
+        tableName: "group_invite_codes",
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+        sequelize,
+        updatedAt: false,
+      }
+    );
+  }
+  static associate(db) {
+    db.GroupInviteCode.belongsTo(db.Group);
+    db.GroupInviteCode.belongsTo(db.User);
+  }
+}
+
+module.exports = GroupInviteCode;

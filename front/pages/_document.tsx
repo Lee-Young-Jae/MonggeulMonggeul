fadbdea46171a16b3890b5c2157338fb945ff1bb
Lee-Young-Jae <yjlee1937@naver.com>
2023-09-03 17:57:10 +0900

    feat: 메타 태그 작성

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head></Head>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="
+          모임 관리 서비스, 몽글입니다.
+        "
+        />
+        <meta
+          name="keywords"
+          content="
+          몽글,몽글몽글,몽글몽글몽글,몽글몽글몽글몽글,몽글몽글몽글몽글몽글,몽글몽글몽글몽글몽글몽글,몽글몽글몽글몽글몽글몽글몽글,몽글몽글몽글몽글몽글몽글몽글몽글,몽글몽글몽글몽글몽글몽글몽글몽글몽글,몽글몽글몽글몽글몽글몽글몽글몽글몽글몽글
+        "
+        />
+        <meta name="author" content="몽글" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="몽글" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://moim.monggeul.online" />
+      </Head>
       <body>
         <Main />
         <NextScript />

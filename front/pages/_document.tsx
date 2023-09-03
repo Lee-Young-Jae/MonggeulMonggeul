bc72c423e3d557f0e1573e5d8a8426d568776ffc
Lee-Young-Jae <yjlee1937@naver.com>
2023-09-03 17:50:07 +0900

    fix: 사용하지 않는 메타 태그 제거

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -2,13 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
+    <Html lang="ko">
+      <Head></Head>
       <body>
         <Main />
         <NextScript />

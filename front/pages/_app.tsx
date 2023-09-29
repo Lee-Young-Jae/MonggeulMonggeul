5933543cdab4edab115cd4e473ad5f44f29db4f3
Lee-Young-Jae <yjlee1937@naver.com>
2023-09-30 00:05:36 +0900

    fix: recoil package 제거에 따라 import 구문 수정

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import AppLayout from "../components/layout/AppLayout";
 import "./font.css";
-import { RecoilRoot } from "recoil";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/apis/config/queryClient";
@@ -15,13 +14,11 @@ queryClient.setDefaultOptions({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <AppLayout>
-          <Component {...pageProps} />
-        </AppLayout>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
